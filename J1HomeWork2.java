7a3485fcc056bfb948890c72035a25f207b20197
Mikhail <ohotojie6@gmail.com>
2019-06-13 11:27:29 +0300

    Создал массив и заполнил его кратными 3.

--- J1HomeWork2.java
+++ J1HomeWork2.java
@@ -1,9 +1,22 @@
 public class J1HomeWork2 {
     public static void main(String[] args) {
-        int[] arr1 = {1,0,0,1,0,1,0,1};
-        for (int i = 0; i < arr1.length; i++) {
-            arr1[i] = (arr1[i] == 0 ? 1 : 0);
-            System.out.print(arr1[i] + " ");
+//        1  EXERCISE
+        {
+            int[] arr1 = {1, 0, 0, 1, 0, 1, 0, 1};
+            for (int i = 0; i < arr1.length; i++) {
+                arr1[i] = (arr1[i] == 0 ? 1 : 0);
+                System.out.print(arr1[i] + " ");
+            }
+            System.out.println();
+        }
+//        2 EEXRCISE
+        {
+            int[] arr = new int[8];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = i * 3;
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
         }
 
     }
