fdecc752787e09e42ae34cda0b9d7028cdef9e71
Mikhail <ohotojie6@gmail.com>
2019-06-13 16:07:35 +0300

    Создан массив {1,5,3,2,11,4,5,2,4,8,9,1} все элементы меньше 6 умножены на 2

--- J1HomeWork2.java
+++ J1HomeWork2.java
@@ -1,6 +1,6 @@
 public class J1HomeWork2 {
     public static void main(String[] args) {
-//        1  EXERCISE
+//          1  EXERCISE
         {
             int[] arr1 = {1, 0, 0, 1, 0, 1, 0, 1};
             for (int i = 0; i < arr1.length; i++) {
@@ -9,7 +9,7 @@ public class J1HomeWork2 {
             }
             System.out.println();
         }
-//        2 EEXRCISE
+//          2 EXERCISE
         {
             int[] arr = new int[8];
             for (int i = 0; i < arr.length; i++) {
@@ -18,6 +18,14 @@ public class J1HomeWork2 {
             }
             System.out.println();
         }
-
+//          3 EXERCISE
+        {
+            int[] arr = {1,5,3,2,11,4,5,2,4,8,9,1};
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = (arr[i] < 6 ? arr[i] * 2 : arr[i]);
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
     }
 }
